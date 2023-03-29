02c240b1ae8fbeabba0a4641e7152537db252224
LiloFr <lilo.frich@gmail.com>
2023-03-29 18:20:04 +0300

    keydown change colors

--- colors/src/index.js
+++ colors/src/index.js
@@ -1,11 +1,39 @@
 const columns = document.querySelectorAll('.column');
 
+document.addEventListener('keydown', e =>{
+  if (e.code.toLowerCase() === 'space'){
+    setRandomColors()
+  }
+})
+
 function setRandomColors() {
   columns.forEach(column => {
-    console.log(column);
+    const text = column.querySelector('h2')
+    const button = column.querySelector('button')
+    const color = chroma.random()
+
+    // const color = generateColor()
+
+    text.textContent = color
+    column.style.background = color
+
+    setTextColor(text, color)
+    setTextColor(button, color)
   })
 }
 
 function generateColor(){
-   
+   const hexCodes = '0123456789ABCDEF'
+   let color = ''
+   for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+   }
+   return '#' + color
 }
+
+function setTextColor(text, color){
+  const luminance = chroma(color).luminance()
+  text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+setRandomColors();
